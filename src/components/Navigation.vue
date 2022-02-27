57d59fc44e4f0846b21938bfa6a46ebfaf3cc62d
Edwinruiz2 <92284075+Edwinruiz2@users.noreply.github.com>
2022-02-26 21:11:44 -0600

    Add files via upload

---
+++ src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+      <div>
+  <b-navbar type="blue" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item to ="/">Home</b-nav-item>
+      <b-nav-item to ="/about">About</b-nav-item>
+      <b-nav-item to ="/inicio">Inicio</b-nav-item>
+      <b-nav-item to ="/perros">Perros</b-nav-item>
+      <b-nav-item to ="/gatos">Gatos</b-nav-item>
+       <b-nav-item to ="/conejos">Conejos</b-nav-item>
+       <b-nav-item to ="/loros">Loros</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+    <router-view/>
+    </div>
+</template>
+<script>
+
+export default{
+    name:'Navigation'
+        
+    }
+</script>
